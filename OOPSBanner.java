fe64763cd55c62552d81b99bc001a3fff759cdf1
Rajat Sonee <rajatsonee23@gmail.com>
2026-02-24 15:30:58 +0530

    UC1: Print OOPS to console

---
+++ OOPSBanner.java
@@ -0,0 +1,13 @@
+/**
+ * OOPS Banner App - Use Case 1
+ * @author Rajat Sonee
+ * @version 1.0
+ */
+public class OOPSBanner {
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+    }
+}
+
+
+
